ba8143a2d00c4a22e083cf1cc4ca20220017cee7
Mick Bogaard <mickbogaard@gmail.com>
2020-03-31 18:42:02 +0200

    test

---
+++ OutsideTheBoxGame/Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] string gameText;
+    [SerializeField] State[] otherStates;
+
+    public string GetStateText()
+    {
+        return gameText;
+    }
+
+    public State[] GetOtherStates()
+    {
+        return otherStates;
+    }
+}
