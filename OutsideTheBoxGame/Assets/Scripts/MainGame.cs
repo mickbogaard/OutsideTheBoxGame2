b3c1d95f7eaa1946fb6f6b1ce8d8090c6c1b9230
Mick Bogaard <mickbogaard@gmail.com>
2020-04-01 15:53:23 +0200

    first few states added

--- OutsideTheBoxGame/Assets/Scripts/MainGame.cs
+++ OutsideTheBoxGame/Assets/Scripts/MainGame.cs
@@ -13,39 +13,19 @@ public class MainGame : MonoBehaviour
     public Button buttonMiddle;
     public Button buttonRight;
 
-    // Start is called before the first frame update
     void Start()
     {
         state = startState;
         textElement.text = state.GetStateText();
+        SetButtonText();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //GameMain();
-    }
-
-    //used to respond to input
-    /*private void GameMain()
-    {
-        State[] statesArray = state.GetOtherStates();
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            state = statesArray[0];
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            state = statesArray[1];
-        }
-        textElement.text = state.GetStateText();
-    }*/
 
     public void leftClick()
     {
         State[] statesArray = state.GetOtherStates();
         state = statesArray[0];
         textElement.text = state.GetStateText();
+        SetButtonText();
     }
 
     public void middleClick()
@@ -53,6 +33,7 @@ public class MainGame : MonoBehaviour
         State[] statesArray = state.GetOtherStates();
         state = statesArray[1];
         textElement.text = state.GetStateText();
+        SetButtonText();
     }
 
     public void rightClick()
@@ -60,5 +41,34 @@ public class MainGame : MonoBehaviour
         State[] statesArray = state.GetOtherStates();
         state = statesArray[2];
         textElement.text = state.GetStateText();
+        SetButtonText();
+    }
+
+    public void Update()
+    {
+        if (buttonLeft.GetComponentInChildren<Text>().text == "")
+        {
+            buttonLeft.gameObject.SetActive(false);
+        }
+        else buttonLeft.gameObject.SetActive(true);
+
+        if (buttonMiddle.GetComponentInChildren<Text>().text == "")
+        {
+            buttonMiddle.gameObject.SetActive(false);
+        }
+        else buttonMiddle.gameObject.SetActive(true);
+
+        if (buttonRight.GetComponentInChildren<Text>().text == "")
+        {
+            buttonRight.gameObject.SetActive(false);
+        }
+        else buttonRight.gameObject.SetActive(true);
+    }
+
+    private void SetButtonText()
+    {
+        buttonLeft.GetComponentInChildren<Text>().text = state.GetButtonTextLeft();
+        buttonMiddle.GetComponentInChildren<Text>().text = state.GetButtonTextMiddle();
+        buttonRight.GetComponentInChildren<Text>().text = state.GetButtonTextRight();
     }
 }
--- OutsideTheBoxGame/Assets/Scripts/State.cs
+++ OutsideTheBoxGame/Assets/Scripts/State.cs
@@ -7,11 +7,29 @@ using UnityEngine.UI;
 public class State : ScriptableObject
 {
     [SerializeField] string gameText;
+    [SerializeField] string buttonTextLeft;
+    [SerializeField] string buttonTextMiddle;
+    [SerializeField] string buttonTextRight;
     [SerializeField] State[] otherStates;
 
     public string GetStateText()
     {
         return gameText;
+    }
+
+    public string GetButtonTextLeft()
+    {
+        return buttonTextLeft;
+    }
+
+    public string GetButtonTextMiddle()
+    {
+        return buttonTextMiddle;
+    }
+
+    public string GetButtonTextRight()
+    {
+        return buttonTextRight;
     }
 
     public State[] GetOtherStates()
