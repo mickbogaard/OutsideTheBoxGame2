ecf015bc202ef5a1749e62d35865e1de5eddd9b6
Mick Bogaard <mickbogaard@gmail.com>
2020-04-04 13:46:26 +0200

    Added main menu to the game

---
+++ OutsideTheBoxGame/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private SceneSwitcher _sceneSwitcher;
+
+    void Start()
+    {
+        Debug.Log("startup");
+        _sceneSwitcher = new SceneSwitcher();
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("start");
+        _sceneSwitcher.LoadScene("Main");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+
+}
